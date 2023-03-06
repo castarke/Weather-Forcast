a92fede21f6a8f76c0ef9961c16f5e3ff4ff4980
castarke <candler.starke@yahoo.com>
2023-03-05 19:33:31 -0500

    fixed the issue where the searched cities were not displaying on the page

--- assets/script.js
+++ assets/script.js
@@ -20,8 +20,8 @@ var futureApiKey = 'f443a7bf0556b76f0d415dce60061886'
 // var lang = 'en'
 var city = document.getElementById('citySearch')
 var previousSearched = document.querySelector("#searched")
-var previousCityArray = [];
-var previousSearchesList = document.querySelector("#searched .list-group-item")
+var previousCityArray = JSON.parse(localStorage.getItem('previousCities')) || [];
+var previousSearchesList = document.querySelector("#searched .list-group")
 
 function getUrl(testCity){
     var apiLink = `https://api.openweathermap.org/data/2.5/forecast?q=${testCity}&units=imperial&lang=en&appid=f443a7bf0556b76f0d415dce60061886`;
@@ -35,85 +35,53 @@ function getUrl(testCity){
             console.log(data[i]);
         }
     });
+    
 }
 getUrl()
 
-let earlySearch= JSON.parse(localStorage.getItem("searched")) ||[];
 
-if (earlySearch !== null) {
-    for (let i = 0; i < earlySearch.length; i++) {
-        if (earlySearch[i] === null) {
-            earlySearch.splice(i, i+1);
-        } else {
-            // Populates previous to publish previous search buttons
-            previousCityArray.push(earlySearch[i]);
-        }
-    }
+function renderPreviousSearches() {
+    // Clear previous searches list
+    previousSearchesList.innerHTML = "";
+  
+    // Create a set of unique cities
+    var uniqueCities = new Set(previousCityArray);
+  
+    // Loop through unique cities and create list item for each city
+    uniqueCities.forEach(function(city) {
+        var listItem = document.createElement("li");
+        listItem.classList.add("list-group-item");
+        listItem.textContent = city;
+        previousSearchesList.appendChild(listItem);
+      
+        // Add click listener to list item
+        listItem.addEventListener("click", function() {
+            getUrl(city);
+        });
+    });
 }
 
-function searchHistoryRefresh() {
-    // Pulls localStorage results of previous searches
-    earlySearch = JSON.parse(localStorage.getItem("searched"));
+// Call renderPreviousSearches() once when the page loads to render the initial list of previous searches
+renderPreviousSearches();
 
-    // Declared under function to ensure list is updated each time
-    var existingCities = document.querySelectorAll("#searched list-group-item button");
-
-    if (finder !== null) {
-        existingCities.forEach(function(button) {
-            // Ensures buttons aren't repeated for existing searches
-            for (let i = 0; i < finder.length; i++)
-            if (button.dataset.city.includes(finder[i])) {
-                finder.splice(i, i + 1);
-            }
-        })
-        for (let i = 0; i < finder.length; i++) {
-            var searchBtn = document.createElement("button");
-            searchBtn.classList.add("m-2", "btn", "btn-light");
-            // Sets data-city attribute on button for event listener to reference
-            searchBtn.dataset.city = finder[i];
-            searchBtn.textContent = finder[i];
-            searchBtn.addEventListener("click", (event) => {
-                // References data-city property to call API
-                // callOpenWeather(event.target.dataset.city);
-            })
-            previousSearchesList.appendChild(searchBtn); 
-        }
-    }
-}
-
-var updateCities = (city) => {
-    // Ensures searched city isn't pushed into array (and then localStorage) if city has already been searched
-    if (previousCityArray.includes(city)) {
-        return;
-    } else {
-        previousCityArray.push(city);
-        // Stores for next user visit
-        localStorage.setItem("searched", JSON.stringify(previousCityArray));
-        
-        // Calls searchHistoryRefresh to add new search to previous search buttons
-        searchHistoryRefresh();
-    }
-}
 $(".searchBtn").on('click',function(){
-    localStorage.setItem(citySearch.value ,date)
-    console.log(citySearch.value);
-    var testCity = citySearch.value;
-    getUrl(testCity)
-})
+    var newCity = previousCityArray.indexOf(citySearch.value);
+    if (newCity >= 0) {
+        // City already exists in the array, do not add it again
+        console.log('City already exists in the array');
+    } else {
+        // City does not exist in the array, add it
+        previousCityArray.push(citySearch.value);
+        localStorage.setItem('previousCities', JSON.stringify(previousCityArray));
+        console.log(citySearch.value);
+        var testCity = citySearch.value;
+        getUrl(testCity);
+        renderPreviousSearches();
+    }
+});
 
 var date = dayjs().format('dddd, MMMM D YYYY');
 $("#currentTime").html(date);
-
-// function getApi(requestCurrentWeather , requestFutureWeather){
-//     fetch(requestCurrentWeather , requestFutureWeather)
-
-//         .then(function(response){
-//             return response.json();
-//         })
-//         .then(function(data){
-//             console.log(data);
-//         })
-// }
 
 
 document.querySelector("#citySearch").value;
@@ -122,3 +90,64 @@ event.preventDefault();
 
 console.log(citySearch.value);
 })
+
+
+
+
+// // Pulling earlier searches from local storage
+// let earlySearch= JSON.parse(localStorage.getItem("searched")) ||[];
+
+// if (earlySearch !== null) {
+//     for (let i = 0; i < earlySearch.length; i++) {
+//         if (earlySearch[i] === null) {
+//             earlySearch.splice(i, i+1);
+//         } else {
+//             // Populates previous to publish previous search buttons
+//             previousCityArray.push(earlySearch[i]);
+//         }
+//     }
+// }
+
+// function searchHistoryRefresh() {
+//     // Pulls localStorage results of previous searches
+//     earlySearch = JSON.parse(localStorage.getItem("searched"));
+
+//     // Declared under function to ensure list is updated each time
+//     var existingCities = document.querySelectorAll("#searched list-group-item button");
+
+//     if (finder !== null) {
+//         existingCities.forEach(function(button) {
+//             // Ensures buttons aren't repeated for existing searches
+//             for (let i = 0; i < finder.length; i++)
+//             if (button.dataset.city.includes(finder[i])) {
+//                 finder.splice(i, i + 1);
+//             }
+//         })
+//         for (let i = 0; i < finder.length; i++) {
+//             var searchBtn = document.createElement("button");
+//             searchBtn.classList.add("m-2", "btn", "btn-light");
+//             // Sets data-city attribute on button for event listener to reference
+//             searchBtn.dataset.city = finder[i];
+//             searchBtn.textContent = finder[i];
+//             searchBtn.addEventListener("click", (event) => {
+//                 // References data-city property to call API
+//                 // callOpenWeather(event.target.dataset.city);
+//             })
+//             previousSearchesList.appendChild(searchBtn); 
+//         }
+//     }
+// }
+
+// var updateCities = (city) => {
+//     // Ensures searched city isn't pushed into array (and then localStorage) if city has already been searched
+//     if (previousCityArray.includes(city)) {
+//         return;
+//     } else {
+//         previousCityArray.push(city);
+//         // Stores for next user visit
+//         localStorage.setItem("searched", JSON.stringify(previousCityArray));
+        
+//         // Calls searchHistoryRefresh to add new search to previous search buttons
+//         searchHistoryRefresh();
+//     }
+// }
