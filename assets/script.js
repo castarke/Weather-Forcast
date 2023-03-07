d9c38efd373c135623ed9a8a9a36fb33afd53af8
castarke <candler.starke@yahoo.com>
2023-03-06 23:30:48 -0500

    fixed typo that was hendering the icons to update

--- assets/script.js
+++ assets/script.js
@@ -65,7 +65,7 @@ function renderPreviousSearches() {
         listItem.addEventListener("click", function() {
             getUrl(city);
             getCurrentUrl(city);
-r        });
+       });
     });
 }
 
@@ -113,6 +113,7 @@ $("#current-date").text("Date: " + date);
 $("#current-wind").text("Speed (mph): " + currentDay.wind.speed);
 $("#current-humidity").text("Humidity: " + currentDay.main.humidity);
 $("#current-img").attr("src", `https://openweathermap.org/img/wn/${currentDay.weather[0].icon}.png`);
+
 }
 
 
@@ -125,7 +126,7 @@ $("#forcast1-climate").text(dayOne.weather[0].description);
 $("#forcast1-date").text("Date: " +dayOne.dt_txt);
 $("#forcast1-wind").text("Speed (mph)" + dayOne.wind.speed);
 $("#forcast1-humidity").text("Humidity: " + dayOne.main.humidity);
-$("forcast1-img").attr("src", `https://openweathermap.org/img/wn/${dayOne.weather[0].icon}.png`);
+$("#forcast1-img").attr("src", `https://openweathermap.org/img/wn/${dayOne.weather[0].icon}.png`);
 
 var dayTwo =data.list[8];
 $("#forcast2-city").text("Current City: "+ data.city.name);
@@ -134,7 +135,7 @@ $("#forcast2-climate").text(dayTwo.weather[0].description);
 $("#forcast2-date").text("Date: " +dayTwo.dt_txt);
 $("#forcast2-wind").text("Speed (mph)" + dayTwo.wind.speed);
 $("#forcast2-humidity").text("Humidity: " + dayTwo.main.humidity);
-$("forcast2-img").attr("src", `https://openweathermap.org/img/wn/${dayTwo.weather[0].icon}.png`);
+$("#forcast2-img").attr("src", `https://openweathermap.org/img/wn/${dayTwo.weather[0].icon}.png`);
 
 var dayThree =data.list[16];
 $("#forcast3-city").text("Current City: "+ data.city.name);
@@ -143,7 +144,7 @@ $("#forcast3-climate").text(dayThree.weather[0].description);
 $("#forcast3-date").text("Date: " +dayThree.dt_txt);
 $("#forcast3-wind").text("Speed (mph)" + dayThree.wind.speed);
 $("#forcast3-humidity").text("Humidity: " + dayThree.main.humidity);
-$("forcast3-img").attr("src", `https://openweathermap.org/img/wn/${dayThree.weather[0].icon}.png`);
+$("#forcast3-img").attr("src", `https://openweathermap.org/img/wn/${dayThree.weather[0].icon}.png`);
 
 var dayFour = data.list[24]
 $("#forcast4-city").text("Current City: "+ data.city.name);
@@ -152,7 +153,7 @@ $("#forcast4-climate").text(dayFour.weather[0].description);
 $("#forcast4-date").text("Date: " +dayFour.dt_txt);
 $("#forcast4-wind").text("Speed (mph)" + dayFour.wind.speed);
 $("#forcast4-humidity").text("Humidity: " + dayFour.main.humidity);
-$("forcast4-img").attr("src", `https://openweathermap.org/img/wn/${dayFour.weather[0].icon}.png`);
+$("#forcast4-img").attr("src", `https://openweathermap.org/img/wn/${dayFour.weather[0].icon}.png`);
 
 var dayFive = data.list[32]
 $("#forcast5-city").text("Current City: "+ data.city.name);
@@ -161,7 +162,8 @@ $("#forcast5-climate").text(dayFive.weather[0].description);
 $("#forcast5-date").text("Date: " +dayFive.dt_txt);
 $("#forcast5-wind").text("Speed (mph)" + dayFive.wind.speed);
 $("#forcast5-humidity").text("Humidity: " + dayFive.main.humidity);
-$("forcast5-img").attr("src", `https://openweathermap.org/img/wn/${dayOne.weather[0].icon}.png`);
+$("#forcast5-img").attr("src", `https://openweathermap.org/img/wn/${dayFive.weather[0].icon}.png`);
+
 }
 
 
